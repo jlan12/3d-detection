354b9aa78034003f6e237fecd01d3b2fedb26205
inisovivax <richard.gaoanhs@gmail.com>
2019-12-06 19:15:01 +0000

    add dim model info

--- dim_model/driver_dims.py
+++ dim_model/driver_dims.py
@@ -9,6 +9,8 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input,Conv2D,Dense,Dropout,Flatten, BatchNormalization, Input, GlobalAveragePooling2D, concatenate, MaxPool2D, LeakyReLU, Lambda, add
 import tensorflow.keras.layers as layers
 import tensorflow as tf
+import json
+import numpy as np
 
 K.set_floatx('float32')
 config = tf.compat.v1.ConfigProto()
@@ -17,6 +19,8 @@ session = tf.compat.v1.Session(config=config)
 BATCH_SIZE = 4
 _EPSILON = 1e-7
 SECTORS = 32
+PREDICT_BATCH_SIZE = 16
+NUM_INSTANCES = 40570
 
 def train():
     model = xmodel.getModel()
@@ -45,10 +49,51 @@ def train_from_8_ckpt(ckpt, only_vehicles = True):
     tb = callbacks.TensorBoard(log_dir='./logs')
     backbone.fit_generator(generator, steps_per_epoch=int(36450/BATCH_SIZE), epochs=100, verbose=1, validation_freq=1, validation_data=valgenerator,validation_steps=int(4050/BATCH_SIZE), callbacks=[save_callback,tb])
 
+#Generates a json file with the predicted categories in the output_path location
+#Uses the instance ids from data json as keys and the predicted category as values
+def detect(model_ckpt, output_path):
+    model = load_model(model_ckpt)
+    generator = preprocessor.getDetectionLoader(batchsize = PREDICT_BATCH_SIZE)
+    steps = 0
+    predictions = {}
+
+    for generator_output in generator: # for each image id
+        
+        image_ids = generator_output[1]
+        model_inputs = generator_output[0]
+        
+        batch_predictions = model.predict(model_inputs)
+        steps += 1
+        
+        print("Step: " + str(steps))
+        
+        #Find the maximum value to use as the predicted category
+        for i in range(PREDICT_BATCH_SIZE):
+            predictions[image_ids[i]] = np.asarray(batch_predictions[i]).tolist()
+        
+        #Stop inferencing after passing through entire validation set
+        if steps > (NUM_INSTANCES / 10 / PREDICT_BATCH_SIZE) + 1:
+            break
+    
+    with open (output_path, 'w') as fp:
+        json.dump([predictions], fp)
+    
     
 if __name__ =='__main__':
+    '''Run the driver without arguments to train from scratch.
+    If training from a checkpoint that used 8 sectors, pass in the checkpoint as
+    the first and only argument'''
+    
     if len(sys.argv) > 1:
-        ckpt = sys.argv[1]
-        train_from_8_ckpt(ckpt)
+        if sys.argv[1] != "detect":
+            ckpt = sys.argv[1]
+            train_from_8_ckpt(ckpt)
+        else:
+            if len(sys.argv) == 4:
+                model_ckpt = sys.argv[2]
+                output_path = sys.argv[3]
+                detect(model_ckpt, output_path)
+            else:
+                print("Run inference using following format: python3 driver.py detect model_ckpt output_path")
     else:
         train()
--- dim_model/prep_dim.py
+++ dim_model/prep_dim.py
@@ -95,3 +95,69 @@ def getLoader(mode='training',
                 raise Exception("Suspected Concurrency Error")
             
             
+#Loader for inferencing
+def getDetectionLoader(ry_cats = NUMBER_OF_ROT_Y_SECTORS, 
+              split = 0.1,
+              image_dir='../../home/ubuntu/kitti-3d-detection-unzipped/training/image_2',
+              batchsize=4,
+              jsonfp = "inputs/train_data.json", 
+              segs_dir = "inputs/instance_segs/",
+             discard_cls=[0,1,2,4,7]):
+    
+    annos = json.load(open(jsonfp, mode='r'))
+    # filter and keep the category_ids in lamda/
+    annos = annos if not discard_cls else list(filter(lambda anno:anno['category_id'] not in discard_cls, annos))
+    #Only predict on validation
+    annos = annos[round(len(annos)*(1-split)):]
+    '''
+    Recommendations : [0,1,2,4,7] for all vehicles
+    [0,1,2,3,4,5,7,8] for only cars
+    [1,3,5,6,7,8] for peoples
+    '''
+    while(True):
+        batch_stacked_inputs = [] #src image stacked with segmentation for specific instance
+        batch_view_angles = []
+        instance_ids = []
+        for anno in annos:
+            #Full image
+            image_name = str(anno['image_id']).zfill(6) # lengthen image_id to 6 digits by adding preceding zeros
+            image = cv2.imread(os.path.join(image_dir,str(image_name)+'.png'))
+            
+            #Segmentation
+            segmentation = {}
+            with open (os.path.join(segs_dir, str(anno['id']) + '.txt'), "r") as seg_file:
+                seg_info = seg_file.read()
+                seg_info = seg_info.split("\n")
+                segmentation["counts"] = seg_info[0]
+                segmentation["size"] = [int(dim) for dim in seg_info[1].strip('][').split(",")] #Turn string representation of int list into actual list of integers
+                
+            segmentation = mask.decode(segmentation) # get seg in numpy format
+            
+            segmentation = resize_fit_rect(segmentation, IMAGE_SIZE)
+            image = resize_fit_rect(image, IMAGE_SIZE)
+            segmentation = np.resize(segmentation, (IMAGE_SIZE[0], IMAGE_SIZE[1], 1)) #Add channel to enable concatenation
+            stacked = np.concatenate([image,segmentation],axis=-1)
+            batch_stacked_inputs.append(stacked)
+            
+            bbox = anno['bbox'] # bbox is a list in [x,y,w,h] format
+            #Calculate center of object by taking center of bounding box
+            center = (bbox[0] + bbox[2]/2, bbox[1] + bbox[3]/2)
+            width = image.shape[1]
+            #Calculate angle of view
+            view_angle = center[0] / width * VIEW_ANGLE_TOTAL_X - (VIEW_ANGLE_TOTAL_X / 2)
+            batch_view_angles.append(view_angle)
+            
+            instance_ids.append(anno['id'])
+            
+            if (len(batch_stacked_inputs) == batchsize):
+                batch_stacked_inputs = np.asarray(batch_stacked_inputs)
+                batch_view_angles = np.asarray(batch_view_angles)
+                yield ([batch_stacked_inputs, batch_view_angles], instance_ids)
+                batch_stacked_inputs = []
+                batch_view_angles = []
+                instance_ids = []
+                
+            if (len(batch_stacked_inputs)>batchsize):
+                #Should never happen
+                raise Exception("Suspected Concurrency Error")
+            
