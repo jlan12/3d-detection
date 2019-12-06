a944f68206f2fe7cc0cc82de407164bf059bf66d
Raymond Tu <hh2@umd.edu>
2019-12-06 22:40:33 +0000

    updated to run with user paths

--- full_img_alpha_model/driver.py
+++ full_img_alpha_model/driver.py
@@ -18,39 +18,46 @@ config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
 BATCH_SIZE = 4
-PREDICT_BATCH_SIZE = 16
+PREDICT_BATCH_SIZE = 5
 _EPSILON = 1e-7
 SECTORS = 32
 NUM_INSTANCES = 40570 #Total number of instances, before accounting for val split or discarded classes
 
 #Trains model from scratch, with quality-aware loss
-def train():
-    model = xmodel.getModel()
-    generator = preprocessor.getLoader(batchsize=BATCH_SIZE)
-    valgenerator = preprocessor.getLoader(mode='validation',batchsize=BATCH_SIZE)
+def train(img_dir, annotation_json_fp, segmentation_dir, num_sectors, vehicle_only):
+    num_sectors=int(num_sectors)
+    model = xmodel.getModel(ry_cats=num_sectors)
+    if vehicle_only.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']:
+        generator = preprocessor.getLoader(image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE, discard_cls = [0,1,2,4,7])
+        valgenerator = preprocessor.getLoader(mode='validation', image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE, discard_cls = [0,1,2,4,7])
+    else:
+        generator = preprocessor.getLoader(image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE)
+        valgenerator = preprocessor.getLoader(mode='validation', image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE)
+
     #sgd = optimizers.SGD(learning_rate=0.05, momentum=0.01, nesterov=True)
     nadam = optimizers.Nadam()# apparently according to keras, the default is the recommended value
     model.compile(optimizer='nadam', loss=quality_loss)
-    save_callback = callbacks.ModelCheckpoint('ckpts/weights.{epoch:d}-{loss:.2f}-{val_loss:.2f}.hdf5')# add validation
+    save_callback = callbacks.ModelCheckpoint('./ckpts/weights.{epoch:d}-{loss:.2f}-{val_loss:.2f}.hdf5')# add validation
     tb = callbacks.TensorBoard(log_dir='./logs')
     model.fit_generator(generator, steps_per_epoch=int(36450/BATCH_SIZE), epochs=100, verbose=1, validation_freq=1, validation_data=valgenerator,validation_steps=int(4050/BATCH_SIZE), callbacks=[save_callback,tb])
     
 
 #Trains model from a checkpoint with output vector of size 8
-def train_from_8_ckpt(ckpt, only_vehicles = True):
-    backbone = load_model(ckpt)
+def train_from_ckpt(img_dir, annotation_json_fp, segmentation_dir, num_sectors, vehicle_only, ckpt_fp):
+    num_sectors = int(num_sectors)
+    backbone = load_model(ckpt_fp, custom_objects = {'quality_loss': quality_loss})
     backbone.layers.pop()
-    predictions = Dense(SECTORS, activation = 'softmax', name="final_layer")(backbone.layers[-1].output)
-    model = Model(backbone.input,outputs=predictions)
-    if only_vehicles:
-        generator = preprocessor.getLoader(batchsize=BATCH_SIZE, discard_cls = [0,1,2,4,7])
-        valgenerator = preprocessor.getLoader(mode='validation',batchsize=BATCH_SIZE, discard_cls = [0,1,2,4,7])
+    predictions = Dense(num_sectors, activation = 'softmax', name="final_layer")(backbone.layers[-1].output)
+    model = Model(backbone.input, outputs=predictions)
+    if vehicle_only.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']:
+        generator = preprocessor.getLoader(image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE, discard_cls = [0,1,2,4,7])
+        valgenerator = preprocessor.getLoader(mode='validation', image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE, discard_cls = [0,1,2,4,7])
     else:
-        generator = preprocessor.getLoader(batchsize=BATCH_SIZE)
-        valgenerator = preprocessor.getLoader(mode='validation',batchsize=BATCH_SIZE)
+        generator = preprocessor.getLoader(image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE)
+        valgenerator = preprocessor.getLoader(mode='validation', image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=BATCH_SIZE)
     #sgd = optimizers.SGD(learning_rate=0.05, momentum=0.01, nesterov=True)
     nadam = optimizers.Nadam()
-    model.compile(optimizer='nadam', loss=base_categorical_crossentropy)
+    model.compile(optimizer='nadam', loss=quality_loss)
     save_callback = callbacks.ModelCheckpoint('ckpts/weights.{epoch:d}-{loss:.2f}-{val_loss:.2f}-{val_acc:.2f}.hdf5')# add validation
     tb = callbacks.TensorBoard(log_dir='./logs')
     model.fit_generator(generator, steps_per_epoch=int(36450/BATCH_SIZE), epochs=100, verbose=1, validation_freq=1, validation_data=valgenerator,validation_steps=int(4050/BATCH_SIZE), callbacks=[save_callback,tb])
@@ -85,41 +92,35 @@ def quality_loss(targets, output):
     return - tf.math.reduce_sum((target1 * tf.math.log(output) + (1.0 - target2) * tf.math.log(1.0 - output)),
                            axis=1)
 
-def 
-
 
 #Generates a json file with the predicted categories in the output_path location
 #Uses the instance ids from data json as keys and the predicted category as values
-def detect(model_ckpt, output_path):
-    model = load_model(model_ckpt, custom_objects = {'quality_loss': quality_loss})
-    generator = preprocessor.getDetectionLoader(batchsize = PREDICT_BATCH_SIZE)
-    steps = 0
+def detect(img_dir, annotation_json_fp, segmentation_dir, num_sectors, vehicle_only, ckpt_fp, output_json_path):
+    num_sectors = int(num_sectors)
+    
+    model = load_model(ckpt_fp, custom_objects = {'quality_loss': quality_loss})
+    if vehicle_only.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']:
+        generator = preprocessor.getDetectionLoader(image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=PREDICT_BATCH_SIZE, discard_cls = [0,1,2,4,7])
+    else:
+        generator = preprocessor.getDetectionLoader(image_dir=img_dir, jsonfp=annotation_json_fp, segs_dir=segmentation_dir, ry_cats=num_sectors, batchsize=PREDICT_BATCH_SIZE)
+        
     predictions = {}
-
+    
     for generator_output in generator: # for each image id
         
         image_ids = generator_output[1]
         model_inputs = generator_output[0]
         
         batch_predictions = model.predict(model_inputs)
-        steps += 1
-        
-        print("Step: " + str(steps))
-        
+        print("Output shape: " + str(batch_predictions.shape))
+
         #Find the maximum value to use as the predicted category
         for i in range(PREDICT_BATCH_SIZE):
             predictions[image_ids[i]] = int(np.where(batch_predictions[i] == np.amax(batch_predictions[i]))[0][0])
         
-        #Stop inferencing after passing through entire validation set
-        if steps > (NUM_INSTANCES / 10 / PREDICT_BATCH_SIZE) + 1:
-            break
     
-    with open (output_path, 'w') as fp:
+    with open (output_json_path, 'w') as fp:
         json.dump([predictions], fp)
-    
-
-    
-
     
 if __name__ =='__main__':
     '''Run the driver without arguments to train from scratch.
@@ -127,15 +128,40 @@ if __name__ =='__main__':
     the first and only argument'''
     
     if len(sys.argv) > 1:
-        if sys.argv[1] != "detect":
-            ckpt = sys.argv[1]
-            train_from_8_ckpt(ckpt)
-        else:
-            if len(sys.argv) == 4:
-                model_ckpt = sys.argv[2]
-                output_path = sys.argv[3]
-                detect(model_ckpt, output_path)
+        if sys.argv[1] == 'train':
+            if len(sys.argv) == 7:
+                img_dir = sys.argv[2]
+                annotation_json_fp = sys.argv[3]
+                segmentation_dir = sys.argv[4]
+                num_sectors = sys.argv[5]
+                vehicle_only = sys.argv[6]
+                train(img_dir, annotation_json_fp, segmentation_dir, num_sectors, vehicle_only)
             else:
-                print("Run inference using following format: python3 driver.py detect model_ckpt output_path")
+                print("Run train using following format: python3 driver.py train img_dir annotation_json_fp segmentation_dir num_sectors vehicle_only")
+        if sys.argv[1] == 'train_from_ckpt':
+            if len(sys.argv) == 8:
+                img_dir = sys.argv[2]
+                annotation_json_fp = sys.argv[3]
+                segmentation_dir = sys.argv[4]
+                num_sectors = sys.argv[5]
+                vehicle_only = sys.argv[6]
+                ckpt_fp = sys.argv[7]
+                train_from_ckpt(img_dir, annotation_json_fp, segmentation_dir, num_sectors, vehicle_only, ckpt_fp)
+            else:
+                print("Run inference using following format: python3 driver.py train_from_ckpt img_dir annotation_json_fp segmentation_dir num_sectors vehicle_only ckpt_fp")
+        if sys.argv[1] == 'detect':
+            if len(sys.argv) == 9:
+                img_dir = sys.argv[2]
+                annotation_json_fp = sys.argv[3]
+                segmentation_dir = sys.argv[4]
+                num_sectors = sys.argv[5]
+                vehicle_only = sys.argv[6]
+                ckpt_fp = sys.argv[7]
+                output_json_path = sys.argv[8]
+                detect(img_dir, annotation_json_fp, segmentation_dir, num_sectors, vehicle_only, ckpt_fp, output_json_path)
+            else:
+                print("Run inference using following format: python3 driver.py detect img_dir annotation_json_fp segmentation_dir num_sectors vehicle_only ckpt_fp output_json_path")
     else:
-        train()
+        print("Run train using following format: python3 driver.py train img_dir annotation_json_fp segmentation_dir num_sectors vehicle_only")
+        print("Run inference using following format: python3 driver.py train_from_ckpt img_dir annotation_json_fp segmentation_dir num_sectors vehicle_only ckpt_fp")
+        print("Run inference using following format: python3 driver.py detect img_dir annotation_json_fp segmentation_dir num_sectors vehicle_only ckpt_fp output_path")
--- full_img_alpha_model/preprocessor.py
+++ full_img_alpha_model/preprocessor.py
@@ -84,7 +84,7 @@ def getLoader(mode='training',
             if left == -1:
                 left = ry_cats-1
                     
-            for i in range(cat_num):
+            for i in range(int(cat_num)):
                 distr_cats[right] = 1 - 1/(cat_num + 1) * (i+1)
                 distr_cats[left] = 1 - 1/(cat_num + 1) * (i+1)
                 right += 1
@@ -126,6 +126,7 @@ def getDetectionLoader(ry_cats = NUMBER_OF_ROT_Y_SECTORS,
              discard_cls=[]):
     
     annos = json.load(open(jsonfp, mode='r'))
+    print("Number of full annos: " + str(len(annos)))
     # filter and keep the category_ids in lamda/
     annos = annos if not discard_cls else list(filter(lambda anno:anno['category_id'] not in discard_cls, annos))
     '''
@@ -133,10 +134,17 @@ def getDetectionLoader(ry_cats = NUMBER_OF_ROT_Y_SECTORS,
     [0,1,2,3,4,5,7,8] for only cars
     [1,3,5,6,7,8] for peoples
     '''
-    while(True):
-        batch_stacked_inputs = [] #src image stacked with segmentation for specific instance
-        batch_view_angles = []
-        batch_ry = [] # rot_y values
+    instances = len(annos)
+    print("Number of annos: " + str(instances))
+    
+    steps = 0
+    
+    batch_stacked_inputs = [] #src image stacked with segmentation for specific instance
+    batch_view_angles = []
+    batch_image_ids = []
+    
+    for i in range(2):
+
         for anno in annos:
             #Full image
             image_name = str(anno['image_id']).zfill(6) # lengthen image_id to 6 digits by adding preceding zeros
@@ -151,35 +159,11 @@ def getDetectionLoader(ry_cats = NUMBER_OF_ROT_Y_SECTORS,
                 segmentation["size"] = [int(dim) for dim in seg_info[1].strip('][').split(",")] #Turn string representation of int list into actual list of integers
                 
             segmentation = mask.decode(segmentation) # get seg in numpy format
+            image = resize_fit_rect(image, IMAGE_SIZE)
+            segmentation = resize_fit_rect(segmentation, IMAGE_SIZE)
+            segmentation = np.resize(segmentation, (IMAGE_SIZE[0], IMAGE_SIZE[1], 1)) #Add channel to enable concatenation
+            stacked = np.concatenate([image,segmentation],axis=-1)
             batch_stacked_inputs.append(stacked)
-            
-            categorical_ry = np.zeros(ry_cats) # initialize a vector of size ry_cats
-            distr_cats = np.zeros(ry_cats)
-            section_number = anno['ry_cats'][str(ry_cats)+'_cat'] # get the section number
-            categorical_ry[section_number] = 1
-            distr_cats[section_number] = 1
-            
-            #Spread out the value for quality-aware loss
-            cat_num = (int(ry_cats - 2)/2) # Remove the 1 and 0 sector, and divide by 2
-            #
-            left = section_number - 1
-            right = section_number + 1
-            if right == ry_cats:
-                right = 0
-            if left == -1:
-                left = ry_cats-1
-                    
-            for i in range(cat_num):
-                distr_cats[right] = 1 - 1/(cat_num + 1) * (i+1)
-                distr_cats[left] = 1 - 1/(cat_num + 1) * (i+1)
-                right += 1
-                left -= 1
-                if right == ry_cats:
-                    right = 0
-                if left == -1:
-                    left = ry_cats-1
-            
-            batch_ry.append(np.concatenate((categorical_ry,distr_cats), axis=0))
             
             bbox = anno['bbox'] # bbox is a list in [x,y,w,h] format
             #Calculate center of object by taking center of bounding box
@@ -188,18 +172,20 @@ def getDetectionLoader(ry_cats = NUMBER_OF_ROT_Y_SECTORS,
             #Calculate angle of view
             view_angle = center[0] / width * VIEW_ANGLE_TOTAL_X - (VIEW_ANGLE_TOTAL_X / 2)
             batch_view_angles.append(view_angle)
-            
+            batch_image_ids.append(anno['id']) 
             if (len(batch_stacked_inputs) == batchsize):
                 batch_stacked_inputs = np.asarray(batch_stacked_inputs)
-                batch_ry = np.asarray(batch_ry)
                 batch_view_angles = np.asarray(batch_view_angles)
-                yield ([batch_stacked_inputs, batch_view_angles], batch_ry)
+                
+                yield ([batch_stacked_inputs, batch_view_angles], batch_image_ids)
+
                 batch_stacked_inputs = []
-                batch_ry = []
                 batch_view_angles = []
+                batch_image_ids = []
                 
             if (len(batch_stacked_inputs)>batchsize):
                 #Should never happen
                 raise Exception("Suspected Concurrency Error")
+                
             
             
